971bd06c36981228325a7619ea8adab72748543c
svkhrobbeck <svkhrobbeck@gmail.com>
2023-06-27 00:05:22 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <h1 className="text-3xl font-bold underline bg-black text-white text-center py-3 sm:text-red-600 font-montserrat">Hello world!</h1>;
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        secondary: "00040f",
+        primary: "000f6ff",
+        lightWhite: "rgba(255, 255, 255, 0.7)",
+        lightBlue: "rgba(9, 151, 124, 0.1)",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
+  screens: {
+    xs: "475px",
+    sm: "640px",
+    md: "768px",
+    lg: "1024px",
+    xl: "1280px",
+    "2xl": "1536px",
+  },
+  plugins: [],
+};
