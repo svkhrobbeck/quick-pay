11e2d41dac8bd84c664ce009b453e252508b7659
svkhrobbeck <svkhrobbeck@gmail.com>
2023-06-29 02:38:55 +0500

    Create constants for saving variables

---
+++ src/constants/constants.js
@@ -0,0 +1,6 @@
+export const navLinks = [
+  { title: "Bosh Sahifa", path: "/" },
+  { title: "Xizmatlar", path: "/services" },
+  { title: "Mahsulot", path: "/product" },
+  { title: "Mijozlar", path: "/clients" },
+];
