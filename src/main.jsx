46e5b1d55c345e856fbc6ff95f72d19860f66e99
svkhrobbeck <svkhrobbeck@gmail.com>
2023-06-29 02:37:07 +0500

    Add BrowserRouter to main

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,10 @@ import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
