85a0c8aab469e70c62ae00b964c8876d081834fd
svkhrobbeck <svkhrobbeck@gmail.com>
2023-06-29 02:38:42 +0500

    Create styles inside constants

---
+++ src/constants/styles.js
@@ -0,0 +1,8 @@
+export const styles = {
+  flexCenter: "flex justify-center items-center",
+  flexStart: "flex justify-start items-center",
+  flexBetween: "flex justify-between items-center",
+  container: "container mx-auto px-5",
+  paragraph: "font-montserrat font-normal text-lightWhite text-[18px] sm:text-[30.8px]",
+  heading: "font-montserrat font-semibold text-[40px] sm:text-[48px] text-white leading-[66px] sm:leading-[76px]",
+};
