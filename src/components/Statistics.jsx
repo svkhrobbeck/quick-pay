ff1ed955ba5d60529e6cc8ce75f77d59e0682b7a
svkhrobbeck <svkhrobbeck@gmail.com>
2023-06-29 18:15:41 +0500

    Create statistics component

---
+++ src/components/Statistics.jsx
@@ -0,0 +1,20 @@
+import { statistics } from "../constants/constants";
+
+const Statistics = () => {
+  return (
+    <section className="mb-6 sm:mb-20">
+      <div className="px-5 flex flex-wrap justify-start lg:justify-center items-start">
+        {statistics.map((stat, i) => (
+          <div className="text-white flex justify-start items-center flex-row m-3" key={stat.id}>
+            <h4 className="mr-3 font-montserrat font-semibold text-[30px] sm:text-[35px] lg:text-[42px]">{stat.value}</h4>{" "}
+            <p className="font-montserrat font-medium text-[17px] uppercase sm:text-[22px] bg-gradient-secondary text-transparent bg-clip-text">
+              {stat.title}
+            </p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Statistics;
