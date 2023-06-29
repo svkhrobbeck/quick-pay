762d2df43f71c6883afaf34bcb58481c5e970c26
svkhrobbeck <svkhrobbeck@gmail.com>
2023-06-29 21:20:46 +0500

    Create feature-card component

---
+++ src/components/FeatureCard.jsx
@@ -0,0 +1,20 @@
+import { styles } from "../constants/styles";
+
+const FeatureCard = ({ idx, id, icon, title, content }) => {
+  return (
+    <div
+      className={`flex  flex-row p-6 rounded-[20px] cursor-pointer [&:not(:last-child)]:mb-6 hover:shadow-card transition-all hover:bg-gradient-black duration-300`}
+    >
+      <div className={`w-[64px] h-[64px] mr-3 rounded-full ${styles.flexCenter} bg-lightBlue`}>
+        <img className="w-[50%] h-[50%] object-contain" src={icon} alt={title} />
+      </div>
+
+      <div className="font-montserrat flex-1 flex flex-col">
+        <h4 className="font-semibold text-white text-[18px] leading-[23px] mb-1">{title}</h4>
+        <p className="font-normal text-lightWhite text-[16px] leading-[24px]">{content}</p>
+      </div>
+    </div>
+  );
+};
+
+export default FeatureCard;
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -6,4 +6,5 @@ export { default as Contract } from "./Contract";
 export { default as Billing } from "./Billing";
 export { default as Button } from "./Button";
 export { default as Business } from "./Business";
+export { default as FeatureCard } from "./FeatureCard";
 export { default as Testimonials } from "./Testimonials";
