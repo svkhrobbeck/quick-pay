7618280df792d610b4a7e5e9233090c59a501607
svkhrobbeck <svkhrobbeck@gmail.com>
2023-06-29 18:15:54 +0500

    Create contract component

---
+++ src/components/Contract.jsx
@@ -0,0 +1,29 @@
+import { card } from "../assets";
+import { Button } from "./";
+import { layout, styles } from "../constants/styles";
+
+const Contract = () => {
+  return (
+    <section className={layout.section}>
+      <div className={`${styles.container} ${layout.wrapper}`}>
+        <div className={`${layout.sectionInfo}`}>
+          <h2 className={`${styles.heading2} mb-5`}>
+            Bir necha oson qadamda muqobil
+            <br className="hidden lg:block" />
+            shartnomaga ega bo'ling
+          </h2>
+          <p className={`${styles.paragraph} max-w-[600px] mb-4`}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus dolorum nemo eius debitis labore repellat atque est ea harum
+            dolor.
+          </p>
+          <Button>Boshlash</Button>
+        </div>
+        <div className={`${layout.sectionImage} ${layout.v1}`}>
+          <img className="w-full h-auto" src={card} alt="card" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contract;
