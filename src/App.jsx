3143b5bbb8d86a8c9e14385abc34a5f791f2feee
svkhrobbeck <svkhrobbeck@gmail.com>
2023-06-29 21:16:32 +0500

    Optimize app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 // components
-import { Billing, Business, Contract, Footer, Header, Hero, Statistics } from "./components";
+import { Billing, Business, CTA, Clients, Contract, Footer, Header, Hero, Statistics, Testimonials } from "./components";
 
 const App = () => {
   return (
     <>
       <h1 className="visually-hidden">Quick Pay</h1>
       <Header />
-      <main className="flex-grow-[1] bg-primary">
+      <main className="flex-grow-[1]">
         <Hero />
         <Statistics />
         <Contract />
         <Billing />
         <Business />
+        <Testimonials />
+        <CTA />
+        <Clients />
       </main>
       <Footer />
     </>
