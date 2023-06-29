180b390c38e15883cf687e55428192b3291def7e
svkhrobbeck <svkhrobbeck@gmail.com>
2023-06-29 18:16:04 +0500

    Create business component

---
+++ src/components/Business.jsx
@@ -0,0 +1,27 @@
+import { layout, styles } from "../constants/styles";
+import { Button } from "./";
+
+const Business = () => {
+  return (
+    <></>
+    // <section className={layout.section} id="features">
+    //   <div className={`${styles.container} ${layout.wrapper}`}>
+    //     <div className={`${layout.sectionInfo}`}>
+    //       <h2 className={`${styles.heading2} mb-5`}>
+    //         Biznesingizni rivojlantiring
+    //         <br className="hidden lg:block" />
+    //         Biz bunda ko'mak beramiz!
+    //       </h2>
+    //       <p className={`${styles.paragraph} max-w-[550px] mb-4`}>
+    //         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate dolore expedita beatae magnam corrupti facere totam
+    //         quibusdam cupiditate, voluptas commodi.
+    //       </p>
+    //       <Button>Boshlash</Button>
+    //     </div>
+    //     <div className={`${layout.sectionImage} ${layout.v1}`}></div>
+    //   </div>
+    // </section>
+  );
+};
+
+export default Business;
