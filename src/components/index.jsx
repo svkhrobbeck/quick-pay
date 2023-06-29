08766fe9036d14dc0b5006aeda51a2f948789e49
svkhrobbeck <svkhrobbeck@gmail.com>
2023-06-29 18:16:34 +0500

    Update components

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,8 @@
 export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
 export { default as Hero } from "./Hero";
+export { default as Statistics } from "./Statistics";
+export { default as Contract } from "./Contract";
+export { default as Billing } from "./Billing";
+export { default as Button } from "./Button";
+export { default as Business } from "./Business";
