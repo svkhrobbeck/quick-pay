af12d8696beb5ba2e3bc8757274f24a9677914d5
svkhrobbeck <svkhrobbeck@gmail.com>
2023-08-01 00:08:17 +0500

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+//styles
 import "./index.css";
+
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <App />
+);
